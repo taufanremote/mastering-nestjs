8868b90aeeb97e41cd3e233b1b97a0317aa99a18
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-10 16:31:30 +0700

    Generate Modules, Controllers & Services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
