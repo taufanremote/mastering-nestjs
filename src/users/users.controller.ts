27a54692cf21358b37675c5e976b5ea9ce9d12f8
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-11 07:36:34 +0700

    Create user with repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private userService: UsersService) { }
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {
+        this.userService.create(body.email, body.password)
+    }
+}
