f2cff397af7ef00790921d8ac258123ad49590cf
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-11 14:25:53 +0700

    completed user operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +10,25 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(parseInt(id), body)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,34 @@ export class UsersService {
 
         return this.repo.save(user)
     }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({ id })
+    }
+
+    find(email: string) {
+        return this.repo.find({ where: { email } })
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id)
+
+        if (!user) {
+            throw new Error('User not found!')
+        }
+
+        Object.assign(user, attrs)
+
+        return this.repo.save(user)
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id)
+
+        if (!user) {
+            throw new Error('User not found!')
+        }
+
+        return this.repo.remove(user)
+    }
 }
